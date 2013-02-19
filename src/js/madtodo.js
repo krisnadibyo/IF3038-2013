13bb8626003fae5b00270fdfa2ee1761086566e7
Inov Yomanovian <ynov@live.com>
2013-02-19 12:28:09 +0700

    Enriched element object, added auto rule checking

--- src/js/madtodo.js
+++ src/js/madtodo.js
@@ -1,21 +1,40 @@
+/** MadToDo JS Lib **/
+/* requires: A web browser */
 (function($) {
-    $.$id = function(id) {
-        var e = document.getElementById(id);
-
+    /**
+     * Enrich the element object.
+     * 
+     * @param {Object} e
+     */
+    $.$e = function(e) {
         e.attr = function(attr, val) {
             if (val !== undefined) {
                 this.setAttribute(attr, val);
+                return e;
             }
 
             return this.getAttribute(attr);
         }
 
+        e.removeAttr = function(attr) {
+            this.removeAttribute(attr);
+
+            return e;
+        }
+
         e.addClass = function(c) {
+            var classes = this.className.split(' ');
+            if (classes.indexOf(c) !== -1) {
+                return e;
+            }
+
             if (this.className === '') {
                 this.className += c;
             } else {
                 this.className += ' ' + c;
             }
+        
+            return e;
         }
 
         e.removeClass = function(c) {
@@ -25,14 +44,49 @@
             if (index !== -1) {
                 classes.splice(index, 1);
                 this.className = classes.join(' ');
-
-                return true;
             }
 
-            return false;
+            return e;
+        }
+
+        e.hasClass = function(c) {
+            var classes = this.className.split(' ');
+            return (classes.indexOf(c) !== -1);
+        }
+
+        e.html = function(h) {
+            if (h !== undefined) {
+                this.innerHTML = h;
+                return e;
+            }
+
+            return this.innerHTML;
+        }
+        
+        e.val = function(v) {
+            if (this.val === undefined) {
+                return e;
+            }
+
+            if (v !== undefined) {
+                this.value = v;
+                return e;
+            }
+
+            return this.value;
         }
 
         return e;
+    }
+
+    /**
+     * Shortcut of document.getElementById() with enriched element object.
+     * 
+     * @param {String} id
+     */
+    $.$id = function(id) {
+        var e = document.getElementById(id);
+        return $.$e(e);
     };
 
     $.supportLocalStorage = true;
