63e33d32669a0862419eb85fc5d0d6b639a63bbd
Inov Yomanovian <ynov@live.com>
2013-03-14 12:23:45 +0700

    Added Session

--- core/lib/Session.php
+++ core/lib/Session.php
@@ -1,5 +1,30 @@
 <?php
 class Session
 {
-    
+    public static function init()
+    {
+        session_start();
+    }
+
+    public static function set($key, $val)
+    {
+        $_SESSION[$key] = $val;
+    }
+
+    public static function get($key)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+
+    public static function remove($key)
+    {
+        if (isset($_SESSION[$key])) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    public static function destroy()
+    {
+        session_destroy();
+    }
 }
