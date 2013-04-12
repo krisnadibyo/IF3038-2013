fb7445ef0c9482e3c047f4cfbe7ff1f976c08995
Inov Yomanovian <ynov@live.com>
2013-04-12 09:56:44 +0700

    Touched upload controller

---
+++ src/madtodo/controllers/UploadController.java
@@ -0,0 +1,12 @@
+package madtodo.controllers;
+
+import madtodo.MadController;
+
+public class UploadController extends MadController {
+    ////
+    // url { /upload }
+    @Override
+    public void index() {
+        print404();
+    }
+}
